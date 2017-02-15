6f4dde1bbd08b50a6debc9f4f19f47abf04f54c7
rbelle2017 <chienhua.liu@gmail.com>
2017-02-15 22:04:44 +0800

    first commit

---
+++ hw4_sd.R
@@ -0,0 +1,21 @@
+my_sd <- function(input_vector){
+  vector_sum <- 0
+  vector_len <- 0
+  average_value <- 0
+  for (i in input_vector){
+    vector_sum <- vector_sum + i
+    vector_len <- vector_len + 1
+  }
+  average_value <- vector_sum/vector_len
+  sd_sum <- 0
+  for (i in input_vector){
+    sd_sum <- sd_sum + (i - average_value)^2
+  }
+  sd_value = (sd_sum/(vector_len - 1))^0.5
+  return(sd_value)
+}
+
+
+test_vector <- round(runif(10)*100)
+my_sd(test_vector)
+sd(test_vector)
---
+++ hw4_sort.R
@@ -0,0 +1,25 @@
+my_sort <- function(input_vector,decreasing=FALSE){
+  vector_len=length(input_vector)
+  is_exchange <- FALSE
+  
+  for (i in 1:(vector_len-1)){
+    for (j in (i+1):vector_len){
+      if(decreasing){
+        is_exchange <-input_vector[i]<input_vector[j] 
+      }else{
+        is_exchange <-input_vector[i]>input_vector[j] 
+      }
+      if (is_exchange){
+        temp <- input_vector[i]
+        input_vector[i] <- input_vector[j]
+        input_vector[j] <- temp
+      }
+    }
+  }
+  return (input_vector)
+}
+
+set.seed(87)
+test_vec=ceiling(runif(10)*100)
+my_sort(test_vec)
+my_sort(test_vec,decreasing = TRUE)
